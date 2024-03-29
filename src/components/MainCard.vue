d491090f321061358ca06289e55af1981d68a447
roidfree <calling_dameer@hotmail.com>
2024-03-29 17:08:25 +0000

    scratch build

---
+++ src/components/MainCard.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="main-containter">
+        <div class="container-one">
+            <div class="profile">
+                <img src="..\assets\profile.jpg" alt="">
+                <h3>Dameer</h3>
+                <p>Neuroengineer</p>
+            </div>
+            <div class="social-links">
+                <a href="">
+                    <div class="link">
+                        <img src="../assets/github.png" alt="">
+                        <h2>GitHub</h2>
+                    </div>
+                    <div class="link">
+                        <img src="../assets/linkedin.png" alt="">
+                        <h2>LinkedIn</h2>
+                    </div>
+                    <div class="link">
+                        <img src="../assets/twitter.png" alt="">
+                        <h2>Twitter</h2>
+                    </div>
+                    <div class="link">
+                        <img src="../assets/instagram.png" alt="">
+                        <h2>Instagram</h2>
+                    </div>
+                </a>
+            </div>    
+        </div>
+        <div class="container-two">
+            <div class="about-me">
+                <h1>About Me</h1>
+                <p> Dameer is a Bioengineering student at Imperial College London, 
+                    determined to see how far we can engineer our biology to optimise our lives. 
+                    This interest, however, developed from experiences he faced in his younger years. 
+                    During his A-levels, where he took Further Maths, Maths, Physics, Computer Science 
+                    and an EPQ, he was diagnosed with medical issues that severely hindered his 
+                    self-control. It was here that he became obsessed with "finding a cure" - immersing 
+                    himself in behavioural theories and neuroscience - to better understand his own mind. 
+                    Next, he wrote his extended project qualification (EPQ) about curing such ailments 
+                    in the brain: behaviours like procrastination. Alarmingly, he discovered an 
+                    ever-increasing struggle among humans to battle their own minds. 
+                    In his research, he details that in our digital age, the overwhelming distractions 
+                    we face cause our brains to devolve.
+                    Alongside this, a slow frustration brewed as he journeyed through secondary school, 
+                    seeing his peers quit and drop subjects because they saw themselves as 'inadequate'. 
+                    Even though he felt they were far brighter than himself — it was almost as if they 
+                    had placed themselves "in a box". This sentiment still bothers him to this day: 
+                    strong belief that everyone is capable, but the high barrier of entry of self-belief 
+                    stops people from achieving their goals. This 'friction' needs to be overcome for 
+                    intentional change, and it's only getting more challenging in our distractable age.
+                    Dameer has always been a tinkerer. This is why he founded his school's Robotics Society. 
+                    He also knew many students loved this area but had no idea where to start. Once again, 
+                    it was a perceived high barrier to entry, so creating this society was a way of demystifying 
+                    the complexities and allowing people to nurture their passion. Due to the society's success, 
+                    it was approved to receive the school's extracurricular STEM to fund the projects.    
+                    Similar to this endeavour, he believes in being proactive to problems. 
+                    He noticed the lack of teaching on financial knowledge in the school curricula, 
+                    so he founded the Investment Education Society, where he invited the CEO of Path Financial, 
+                    a financial advisory firm, to give a talk and educate students. 
+                    Now at Imperial, he has combined his passions for neuroscience and engineering to found 
+                    the UK's first university-led Neurotechnology society. He has been the project lead for 
+                    the society's first funded product, which received further grants from startup competitions.
+                    He seeks to spread awareness of the dangers of today's increasingly distracted world, 
+                    shining the light on the importance of self-awareness and harnessing 
+                    new technology to pave the way to a more intentional life and future.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    img {
+        height: 20px;
+    }
+</style>
